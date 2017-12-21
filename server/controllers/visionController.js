94056008399b20274d3ca7f53b7d4c504bd4ed0e
angrha <angrha@gmail.com>
2017-12-21 21:29:10 +0700

    read text image done

--- server/controllers/visionController.js
+++ server/controllers/visionController.js
@@ -26,46 +26,24 @@ class Vision {
         headers : { 'Content-Type': 'application/json'}
       })
     .then( result => {
+      //raw identitas nanti di edit setelah ada multer
+      let rawData = result.data.responses[0].textAnnotations[0].description.split('\n')
+      let NIK = rawData[2].slice(3)
+      let nama = rawData[3]
+      let alamat = rawData[10]
       res.status(200).json({
-        result : result.data
-
+        result : rawData,
+        NIK : NIK,
+        nama : nama,
+        alamat : alamat
       })
     })
-    .catch( err => res.status(500).send(err))
+    .catch( err => {
+      console.log('eroor', err)
+      res.status(500).send(err)
+    })
   }
 
 }
 
 module.exports = Vision
-// router.post('/vision', (req, res) => {
-//   let data = {
-//     "requests": [
-//       {
-//         "image": {
-//           "source": {
-//             "imageUri":
-//               req.body.uri
-//           }
-//         },
-//         "features": [
-//           {
-//             "type": "TEXT_DETECTION",
-//             "maxResults": 1
-//           }
-//         ]
-//       }
-//     ]
-//   }
-//   console.log('req.body: ', req.body)
-//   axios.post(' https://vision.googleapis.com/v1/images:annotate?key=AIzaSyA6tkCsALPbiLlw038YHJ0izByVMcNgwU8', 
-//     data, {
-//       headers: { 'Content-Type': 'application/json'}
-//     })
-//   .then(result => {
-//     console.log(result)
-//     res.send(result.data)
-//   })
-//   .catch(err => {
-//     console.log('err: ', err)
-//   })
-// })
