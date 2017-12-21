60cb9fd0c0eab5aad87a8adc0ff6a02df0d1b137
azhariemuhammad <azharie_muhammad@live.com>
2017-12-22 00:23:26 +0700

    login

---
+++ client/src/components/Login.vue
@@ -0,0 +1,57 @@
+<template>
+<div>
+  <h1>Halaman Login</h1>
+  <!-- <form @submit.prevent="loginBtn">
+    <label for="Username"></label>
+    <input type="text" v-model="user.username" placeholder="username">
+    <br>
+    <label for="Email"></label>
+    <input type="text" v-model="user.email" placeholder="email">
+    <input type="submit" name="submit" value="submit">
+  </form> -->
+  <button id="fbLogin" @click="loginBtn">Login</button>
+     <div id="app" class="component">
+      <webcam ref="webcam"></webcam>
+      <img :src="this.photo" style="width:300px;height:300px; display:none"/>
+      <!-- <button type="button" @click="take_photo">Capture Photo</button> -->
+    </div>
+</div>
+  
+</template>
+
+<script>
+/* eslint-disable */
+import webcam from './WebCam'
+import { mapActions, mapState } from 'vuex'
+export default {
+  name: 'Login',
+  components: {webcam},
+  data: function () {
+    return {
+      photo: null
+    }
+  },
+  computed: mapState([
+    'user'
+  ]),
+  methods: {
+    ...mapActions([
+      'login',
+      'logout'
+    ]),
+    loginBtn: function () {
+      localStorage.setItem('flag', 'login')
+      this.$router.push({name: 'Hompage'})
+      this.take_photo()
+      // this.login(user)
+    },
+    take_photo: function () {
+      this.photo = this.$refs.webcam.capture()
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
